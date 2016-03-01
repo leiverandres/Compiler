d1fcbc9ca180c74f05d91b82a7684bfd3b25eadd
Leiver Andres Campeon <leiverandres04p@hotmail.com>
2016-02-29 22:29:37 -0500

    first Lexer working, tested with test.pas

--- paslex.py
+++ paslex.py
@@ -1,4 +1,5 @@
 import ply.lex as lex
+import re
 
 reserved = {
   'fun' : 'FUN',
@@ -19,15 +20,17 @@ reserved = {
   'break': 'BREAK',
   'and': 'AND',
   'or': 'OR',
-  'not': 'NOT'
+  'not': 'NOT',
+  'true': 'TRUE',
+  'false': 'FALSE'
 }
 
 tokens = [
   'ID',
   'ASIGN',
-  'EQUALs',
+  'EQUALS',
   'INTEGER',
-  'FLOAT',
+  'FLOATVAR',
   'CONST',
   'MINUS',
   'PLUS',
@@ -39,31 +42,82 @@ tokens = [
   'RPAREN', # ')'
   'LSQUARE', # left square bracket '['
   'RSQUARE', # right square brecket ']'
-  'LT',
-  'LE',
-  'GT',
-  'GE',
-  'NE',
-  'COMMA',
-  'SEMI',
-  'COLM'
+  'LT', # less than
+  'LE', # less and equal than
+  'GT', # greater than
+  'GE', # greater and equal than
+  'NE', # not-equal
+  'COMMA', # ,
+  'SEMI', # ;
+  'COLM' # :
 ] + list(reserved.values())
 
-# completely ignored characters
-t_ignore = ' \t'
+# literals = '+-*/,;:()[]'
+
+t_ASIGN = r':='
+t_EQUALS = r'=='
+t_MINUS = r'-'
+t_PLUS = r'\+'
+t_TIMES = r'\*'
+t_DIVIDE = r'/'
+t_STRING = r'\".*\"'
+t_LPAREN = r'\('
+t_RPAREN = r'\)'
+t_LSQUARE = r'\['
+t_RSQUARE = r'\]'
+t_LT = r'<'
+t_LE = r'<='
+t_GT = r'>'
+t_GE = r'>='
+t_NE = r'!='
+t_COMMA = r','
+t_SEMI = r';'
+t_COLM = r':'
+
+def t_ID(t):
+  r'[a-zA-Z_][\w]*'
+  t.type = reserved.get(t.value, 'ID')
+  return t
+
+def t_FLOATVAR(t):
+  r'([1-9]\d*|0)(\.\d+(e[+-]?\d+)?|e[+-]?\d+)'
+  try:
+    t.value = float(t.value)
+  except:
+    print ("Line %d: Error with number %s " % (t.lineno, t.value))
+  return t
+
+def t_INTEGER(t):
+  r'([1-9]\d*|0)'
+  try:
+    t.value = int(t.value)
+  except:
+    print ("Line %d: Number is %s too large for a INTEGER" % (t.lineno, t.value))
+    t.value = 0
+  return t
+
+# def t_CONST(t):
+#   r'const'
+#   pass
 
 def t_NEWLINE(t):
   r'\n+'
   t.lexer.lineno += t.value.count("\n")
 
-def t_ID():
-  pass
+# completely ignored characters
+t_ignore = ' \t'
 
 # Discarded tokens
 def t_COMMENT(t):
   r'\/\*(.|\n)*\*\/'
+  t.lexer.lineno += t.value.count('\n') # just take lines
   pass
 
 def t_error(t):
-  print("Illegal character %s foo %d" % repr(t.value[0]))
+  print("Illegal character %s line: %d" % (repr(t.value[0], t.lineno)))
   t.lexer.skip(1)
+
+lex.lex(debug=1)
+
+if __name__ == '__main__':
+  lex.runmain()
