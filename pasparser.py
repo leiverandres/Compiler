e82f5ec0e2d4e3ebe10427d8055846d036f98d0f
Leiver Andres Campeon <leiverandres04p@hotmail.com>
2016-04-14 11:10:41 -0500

    Added command line options and first ast

--- pasparser.py
+++ pasparser.py
@@ -1,312 +1,382 @@
 # coding: utf-8
 import ply.yacc as yacc
+import re
 from paslex import tokens
+import sys
 
+# -----------------------------------------------
+# AST
+# -----------------------------------------------
+class Node:
+    def __init__(self, type, children=None, leaf=None):
+        self.type = type
+        if children:
+            self.children = children
+        else:
+            self.children = []
+            self.leaf = leaf
+
+    def append(self, node):
+        self.children.append(node)
+
+    def __str__(self):
+        return "<%s>" % name
+
+    def __repr__(self):
+        return "<%s>" % name
+
+# -----------------------------------------------
+# Parser
+# -----------------------------------------------
 precedence = (
-  ('left', 'OR'),
-  ('left', 'AND'),
-  ('left', 'NOT'),
-  ('left', '+', '-'),
-  ('left', '*', '/'),
-  ('right', 'UMINUS'),
-  ('right', 'ELSE')
+    ('left', 'OR'),
+    ('left', 'AND'),
+    ('left', 'NOT'),
+    ('left', '+', '-'),
+    ('left', '*', '/'),
+    ('right', 'UMINUS'),
+    ('right', 'ELSE')
 )
 
 def p_program(p):
-  "program : funlist"
-  pass
+    "program : funlist"
+    p[0] = Node("program", [p[1]])
 
 def p_fun_list(p):
-  "funlist : funlist function"
-  pass
+    "funlist : funlist function"
+    p[1].append(p[2])
+    p[0] = p[1]
 
 def p_funList_function(p):
-  "funlist : function"
-  pass
+    "funlist : function"
+    p[0] = Node("Funlist", [p[1]])
 
 def p_function(p):
-  "function : FUN ID '(' arglist ')' localslist BEGIN statementBlock END"
-  pass
+    "function : FUN ID '(' arglist ')' localslist BEGIN statementBlock END"
+    p[0] = Node("Function", [p[4], p[6], p[8]], p[2])
 
 def p_arglist(p):
-  "arglist : args"
-  pass
+    "arglist : args"
+    p[0] = Node("ArgList", [p[1]])
 
 def p_arglist_empty(p):
-  "arglist : empty"
-  pass
+    "arglist : empty"
+    p[0] = p[1] # may be or not??
 
 def p_args(p):
-  "args : args ',' var_decl"
-  pass
+    "args : args ',' var_decl"
+    p[1].append(p[3])
+    p[0] = p[1]
 
 def p_args_argument(p):
-  "args : var_decl"
-  pass
+    "args : var_decl"
+    p[0] = Node("Args", [p[1]])
 
 def p_var_decl(p):
-  "var_decl : ID ':' type_specifier"
-  pass
+    "var_decl : ID ':' type_specifier"
+    p[0] = Node("VarDecl", [p[3]], p[1])
 
 def p_type_specifier1(p):
-  "type_specifier : type"
-  pass
+    "type_specifier : type"
+    p[0] = p[1]
 
 def p_type_specifier2(p):
-  "type_specifier : FLOAT '[' INTEGER ']'"
-  pass
+    "type_specifier : FLOAT '[' INTEGER ']'"
+    p[0] = Node("Float[index]", leaf=p[3])
 
 def p_type_specifier3(p):
-  "type_specifier : INT '[' INTEGER ']'"
-  pass
+    "type_specifier : INT '[' INTEGER ']'"
+    p[0] = Node("Int[index]", leaf=p[3])
 
 def p_type_int(p):
-  "type : INT"
-  pass
+    "type : INT"
+    p[0] = Node("INT")
 
 def p_type_float(p):
-  "type : FLOAT"
-  pass
+    "type : FLOAT"
+    p[0] = Node("FLOAT")
 
 def p_localList_locals(p):
-  "localslist : locals"
-  pass
+    "localslist : locals"
+    p[0] = p[1]
 
 def p_localList_empty(p):
-  "localslist : empty"
-  pass
+    "localslist : empty"
+    p[0] = p[1]
 
 def p_locals(p):
-  "locals : locals declaration_local ';'"
-  pass
+    "locals : locals declaration_local ';'"
+    p[1].append(p[2])
+    p[0] = p[1]
 
 def p_locals_localDecl(p):
-  "locals : declaration_local ';'"
-  pass
+    "locals : declaration_local ';'"
+    p[0] = p[1]
 
 def p_localDecl_varDecl(p):
-  "declaration_local : var_decl"
-  pass
+    "declaration_local : var_decl"
+    p[0] = Node("Locals declaration", [p[1]])
 
 def p_localDecl_fun(p):
-  "declaration_local : function"
-  pass
+    "declaration_local : function"
+    p[0] = Node("Locals Declaration", [p[1]])
 
 def p_statementBlock(p):
-  "statementBlock : statementBlock ';' statement"
-  pass
+    "statementBlock : statementBlock ';' statement"
+    p[1].append(p[3])
+    p[0] = p[1]
 
 def p_statement(p):
-  "statementBlock : statement"
-  pass
+    "statementBlock : statement"
+    p[0] = Node("Statement Block", [p[1]])
 
 def p_statement_while(p):
-  "statement : WHILE relationop DO statement"
-  pass
+    "statement : WHILE relationop DO statement"
+    p[0] = Node("While", [p[2], p[4]])
 
 def p_statement_ifthen(p):
-  "statement : ifthen"
-  pass
+    "statement : ifthen"
+    p[0] = p[1]
 
 def p_statement_ifThenElse(p):
-  "statement : ifthenelse"
-
+    "statement : ifthenelse"
+    p[0] = p[1]
 
 def p_statement_assign(p):
-  "statement : location ASIGN expression"
-  pass
+    "statement : location ASIGN expression"
+    p[0] = Node("Assigment", [p[1], p[3]])
 
 def p_statement_inOutExpr(p):
-  "statement : inOutExpr"
-  pass
+    "statement : inOutExpr"
+    p[0] = p[1]
 
 def p_statement_return(p):
-  "statement : RETURN expression"
-  pass
+    "statement : RETURN expression"
+    p[0] = Node("RETURN", [p[2]])
 
 def p_statement_call(p):
-  "statement : functionCall"
-  pass
+    "statement : functionCall"
+    p[0] = p[1]
 
 def p_statement_skip(p):
-  "statement : SKIP"
-  pass
+    "statement : SKIP"
+    p[0] = Node("Skip")
 
 def p_statement_break(p):
-  "statement : BREAK"
-  pass
+    "statement : BREAK"
+    p[0] = Node("Break")
 
 def p_statement_block(p):
-  "statement : BEGIN statementBlock END"
-  pass
+    "statement : BEGIN statementBlock END"
+    p[0] = Node("BeginEnd Block", [p[2]])
 
 def p_ifthen(p):
-  'ifthen : IF relationop THEN statement %prec ELSE'
-  pass
+    'ifthen : IF relationop THEN statement %prec ELSE'
+    p[0] = Node("If then", [p[2], p[4]])
 
 def p_ifthenelse(p):
-  'ifthenelse : IF relationop THEN statement ELSE statement'
-  pass
+    'ifthenelse : IF relationop THEN statement ELSE statement'
+    p[0] = Node("If then else", [p[2], p[4], p[5]])
 
 def p_functionCall(p):
-  "functionCall : ID '(' arglist ')'"
-  pass
+    "functionCall : ID '(' arglist ')'"
+    p[0] = Node("Function Call", [p[3]], leaf=p[1])
 
 def p_relationop1(p):
-  "relationop : relationop OR relationop"
-  pass
+    "relationop : relationop OR relationop"
+    p[0] = Node("Relation op", [p[1], p[3]], p[2])
 
 def p_relationop2(p):
-  "relationop : relationop AND relationop"
-  pass
+    "relationop : relationop AND relationop"
+    p[0] = Node("Relation op", [p[1], p[3]], p[2])
 
 def p_relationop3(p):
-  "relationop : NOT relationop"
-  pass
+    "relationop : NOT relationop"
+    p[0] = Node("Relation op", [p[2]], p[1])
 
 def p_relationop4(p):
-  "relationop : '(' relationop ')'"
-  pass
+    "relationop : '(' relationop ')'"
+    p[0] = Node("Relation op", [p[2]])
 
 def p_relationop5(p):
-  "relationop : relation"
-  pass
+    "relationop : relation"
+    p[0] = p[1]
 
 def p_relation_LT(p):
-  "relation : expression LT expression"
-  pass
+    "relation : expression LT expression"
+    p[0] = Node("Relation", [p[1], p[3]], p[2])
 
 def p_relation_LE(p):
-  "relation : expression LE expression"
-  pass
+    "relation : expression LE expression"
+    p[0] = Node("Relation", [p[1], p[3]], p[2])
 
 def p_relation_GT(p):
-  "relation : expression GT expression"
-  pass
+    "relation : expression GT expression"
+    p[0] = Node("Relation", [p[1], p[3]], p[2])
 
 def p_relation_GE(p):
-  "relation : expression GE expression"
-  pass
+    "relation : expression GE expression"
+    p[0] = Node("Relation", [p[1], p[3]], p[2])
 
 def p_relation_NE(p):
-  "relation : expression NE expression"
-  pass
+    "relation : expression NE expression"
+    p[0] = Node("Relation", [p[1], p[3]], p[2])
 
 def p_relation_equal(p):
-  "relation : expression EQUAL expression"
-  pass
+    "relation : expression EQUAL expression"
+    p[0] = Node("Relation", [p[1], p[3]], p[2])
 
 def p_relation_true(p):
-  "relation : TRUE"
-  pass
+    "relation : TRUE"
+    p[0] = Node("True")
 
 def p_relation_false(p):
-  "relation : FALSE"
-  pass
+    "relation : FALSE"
+    p[0] = Node("False")
 
 def p_expr_plus(p):
-  "expression : expression '+' expression"
-  pass
+    "expression : expression '+' expression"
+    p[0] = Node("Expression", [p[1], p[3]], p[2])
 
 def p_expr_minus(p):
-  "expression : expression '-' expression"
-  pass
+    "expression : expression '-' expression"
+    p[0] = Node("Expression", [p[1], p[3]], p[2])
 
 def p_expr_times(p):
  "expression : expression '*' expression"
- pass
+ p[0] = Node("Expression", [p[1], p[3]], p[2])
 
 def p_expr_div(p):
-  "expression : expression '/' expression"
-  pass
+    "expression : expression '/' expression"
+    p[0] = Node("Expression", [p[1], p[3]], p[2])
 
 def p_expr_uminus(p):
-  "expression : '-' expression %prec UMINUS"
-  pass
+    "expression : '-' expression %prec UMINUS"
+    p[0] = Node("Expression", [p[2]], p[1])
 
 def p_expr_uplus(p):
-  "expression : '+' expression"
-  pass
+    "expression : '+' expression"
+    p[0] = Node("Expression", [p[2]])
 
 def p_expr_parens(p):
-  "expression : '(' expression ')'"
-  pass
+    "expression : '(' expression ')'"
+    p[0] = Node("Expression", [p[2]])
 
 def p_expr_id(p):
-  "expression : ID"
-  pass
+    "expression : ID"
+    p[0] = Node("ID")
 
 def p_expr_num(p):
-  "expression : number"
-  pass
-
-def p_expr_id(p):
-  "expression : ID"
-  pass
+    "expression : number"
+    p[0] = p[1]
 
 def p_expr_ubication(p):
-  "expression : ID '[' expression ']'"
+    "expression : ID '[' expression ']'"
+    p[0] = Node("Expression", [p[3]], p[1])
 
 def p_expr_casting(p):
-  "expression : casting"
-  pass
+    "expression : casting"
+    p[0] = p[1]
 
 def p_expr_funcall(p):
-  "expression : functionCall"
-  pass
-
-def p_num_int(p):
-  "number : INTEGER"
-  pass
-
-def p_num_float(p):
-  "number : FLOATNUM"
+    "expression : functionCall"
+    p[0] = p[1]
 
 def p_location1(p):
-  "location : ID"
-  pass
+    "location : ID"
+    p[0] = Node("ID")
 
 def p_location2(p):
-  "location : ID '[' INTEGER ']'"
-  pass
+    "location : ID '[' INTEGER ']'"
+    p[0] = Node("Location", [p[3]], p[1])
 
 def p_casting_int(p):
-  "casting : INTEGER '(' expression ')'"
-  pass
+    "casting : INTEGER '(' expression ')'"
+    p[0] = Node("Casting Int", [p[3]], leaf=int(p[3]))
 
 def p_casting_float(p):
-  "casting : FLOAT '(' expression ')'"
-  pass
+    "casting : FLOAT '(' expression ')'"
+    p[0] = Node("Casting Float", [p[3]], leaf=float(p[3]))
 
 def p_inOutExpr1(p):
-  "inOutExpr : PRINT '(' STRING ')'"
-  pass
+    "inOutExpr : PRINT '(' STRING ')'"
+    p[0] = Node("PRINT", [p[3]])
 
 def p_inOutExpr2(p):
-  "inOutExpr : WRITE '(' expression ')'"
-  pass
+    "inOutExpr : WRITE '(' expression ')'"
+    p[0] = Node("WRITE", [p[3]])
 
 def p_inOutExpr3(p):
-  "inOutExpr : READ '(' expression ')'"
-  pass
+    "inOutExpr : READ '(' expression ')'"
+    p[0] = Node("READ", [p[3]])
+
+def p_number_int(p):
+    "number : INTEGER"
+    p[0] = Node("INTEGER", [p[1]])
+
+def p_number_float(p):
+    "number : FLOATNUM"
+    p[0] = Node("FLOAT", [p[1]])
 
 def p_empty(p):
-  "empty : "
-  pass
+    "empty : "
+    p[0] = Node("Empty")
 
 def p_error(p):
-  print("Error en la entrada!")
+    print("Error en la entrada!")
+    print p
 
-parser = yacc.yacc()
+def make_parser():
+    parser = yacc.yacc(debug=0)
+    return parser
 
-while True:
-  try:
-      s = raw_input('basic > ')
-  except EOFError:
-      break
-  if not s: continue
-  result = parser.parse(s)
-  print(result)
+# ===================================
+# Imprimir AST
+# ===================================
+
+def dump_tree(n, indent = ""):
+
+    if not hasattr(n, "datatype"):
+        datatype = ""
+    else:
+        datatype = n.datatype
+
+    if not n.leaf:
+        print "%s%s %s" % (indent, n.name, datatype)
+    else:
+        print "%s%s (%s) %s" % (indent, n.name, n.leaf, datatype)
+
+    indent = indent.replace("-"," ")
+    indent = indent.replace("+"," ")
+
+    for i in range(len(n.children)):
+        c = n.children[i]
+        if i == len(n.children)-1:
+            dump_tree(c, indent + "+-- ")
+        else:
+            dump_tree(c, indent + "|-- ")
+
+if __name__ == "__main__":
+    if (len(sys.argv) < 2 or len(sys.argv) > 4):
+        print "Usage: python %s [-ast] [-lex] <pascal_file>" % sys.argv[0]
+    elif (re.match(r'.*\.pas', sys.argv[-1])):
+        try:
+            for i in sys.argv:
+                if (i == "-ast"):
+                    print "Get ast op"
+                if (i == "-lex"):
+                    print "Get lex op"
+
+            file = open(sys.argv[-1])
+            data = file.read()
+            parser = make_parser()
+            result = parser.parse(data)
+            print result
+        except IOError:
+            print "Error: The file does not exist"
+            print "Usage: python %s [-ast] [-lex] <pascal_file>" % sys.argv[0]
+    else:
+        print "Please put the name of your pascal file at the end of the command"
 
 '''
 ... refactor
@@ -317,7 +387,7 @@ program: fun_list
 fun_list: fun_list fun
         | fun
 
-fun: FUN ID LPAREN arglist RPAREN localslist BEGIN statementBlock END
+fun: FUN ID '(' arglist ')' localslist BEGIN statementBlock END
 
 arglist: args
        | empty
@@ -346,12 +416,12 @@ declaration_local: var_decl
 statementBlock : statementBlock ';' statement
           | statement
 
-statement: WHILE relationop DO statement ...
-         |  IF relation THEN statement // ifthen ...
-         |  IF relation THEN statement %prec ELSE statement // ifthenelse ...
+statement: WHILE relationop DO statement
+         |  IF relation THEN statement // ifthen
+         |  IF relation THEN statement %prec ELSE statement // ifthenelse
          |  assigment
          |  inOutExpr
-         |  RETURN expression
+         |  RETURN expression ...
          |  functionCall
          |  SKIP
          |  BREAK
@@ -400,5 +470,5 @@ inOutExpr: PRINT LPAREN STRING RPAREN
          | READ LPAREN location RPAREN
 
 casting: FLOAT LPAREN expression RPAREN
-       | ID LPAREN expression RPAREN
+       | INT LPAREN expression RPAREN
 '''
